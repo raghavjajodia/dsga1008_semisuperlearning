cf0ab51ef66d1ab0f87719b9beeb4eb3466f3312
Raghav <raghavjajodia@gmail.com>
2019-05-03 23:17:16 -0400

    bugs

--- supervised_regulaized.py
+++ supervised_regulaized.py
@@ -181,52 +181,55 @@ class Discriminator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is (3) x 64 x 64
-            nn.Conv2d(3, opt.ndf, 4, 2, 1, bias=False),
+            nn.Conv2d(3, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (opt.ndf) x 32 x 32
-            nn.Conv2d(opt.ndf, opt.ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(opt.ndf * 2),
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (opt.ndf*2) x 16 x 16
-            nn.Conv2d(opt.ndf * 2, opt.ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(opt.ndf * 4),
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (opt.ndf*4) x 8 x 8
-            nn.Conv2d(opt.ndf * 4, opt.ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(opt.ndf * 8),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True)
             # state size, (opt.ndf * 8) X 4 X 4
-            nn.Conv2d(opt.ndf * 8, opt.ndf * 16, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(opt.ndf * 16),
+        )
+        self.classif = nn.Sequential(
+            nn.Conv2d(ndf * 8, ndf * 16, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ndf * 16),
             nn.LeakyReLU(0.2, inplace=True),
             # state size, (opt.ndf * 16) X 1 X 1
             Flatten(),
             nn.Dropout(p=0.6),
-            nn.Linear(opt.ndf * 16, opt.ndf * 20),
+            nn.Linear(ndf * 16, ndf * 20),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.8),
-            nn.Linear(opt.ndf * 20, num_class)
-        )            
+            nn.Linear(ndf * 20, num_class)
+        )
 
     def forward(self, input):
         if input.cuda and self.ngpu > 1:
             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+            output2 = nn.parallel.data_parallel(self.classif, output, range(self.ngpu))
         else:
             output = self.main(input)
-        return output
+            output2 = self.classif(output)
+        return output2
 
 netD = Discriminator(opt.ngpu, len(class_names)).to(device)
 netD.load_state_dict(torch.load(opt.net, map_location=device), strict=False)
 
 for param in netD.parameters():
     param.requires_grad = False
-    
-for param in netD.linear.parameters():
+
+for param in netD.classif.parameters():
     param.requires_grad = True
 
-
 criterion = nn.CrossEntropyLoss()
-optimizer_conv = optim.Adam(netD.linear.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999), weight_decay=0.005)
+optimizer_conv = optim.Adam(netD.classif.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999), weight_decay=0.005)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
 netD = train_model(netD, criterion, optimizer_conv, exp_lr_scheduler, device, opt.outf, f, num_epochs=opt.niter)
 torch.save(netD.state_dict(), '%s/netD_best_weights.pth' % (opt.outf))
