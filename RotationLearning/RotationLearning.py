428e831a9e5439b73e43b578903532995b3b80c3
Raghav <raghavjajodia@gmail.com>
2019-05-08 16:47:10 -0400

    bug fix

--- RotationLearning/RotationLearning.py
+++ RotationLearning/RotationLearning.py
@@ -228,7 +228,7 @@ f.flush()
 
 
 #Model Initialization
-model_ft = models.resnet34(pretrained=False)
+model_ft = models.resnet34(pretrained=False).to(device)
 num_ftrs = model_ft.fc.in_features
 model_ft.fc = nn.Linear(num_ftrs, 4)
 
@@ -238,7 +238,7 @@ if opt.netCont !='':
 
 #Model trainer
 criterion = nn.CrossEntropyLoss()
-optimizer_conv = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999), weight_decay=0.005)
+optimizer_conv = optim.Adam(model_ft.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999), weight_decay=0.005)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
 model_ft = train_model(model_ft, criterion, optimizer_conv, exp_lr_scheduler, device, opt.outf, f, num_epochs=opt.niter)
 torch.save(model_ft.state_dict(), '%s/netD_best_weights.pth' % (opt.outf))
